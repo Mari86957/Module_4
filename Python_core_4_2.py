1a1df50c2154dca89ad8f4fc3382dfbe3089e910
Mari86957 <133572710+Mari86957@users.noreply.github.com>
2024-08-02 15:44:35 +0300

    Add files via upload

---
+++ Python_core_4_2.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+cats_info = []
+
+def get_cats_info(path):
+    try:
+        with open("path.txt", "r", encoding='utf-8') as fh:
+            for line in fh:
+                data = line.strip().split(',')
+                my_dict = {
+                    "id": data[0], 
+                    "name": data[1], 
+                    "age": data[2]}
+                cats_info.append(my_dict)
+        return cats_info
+    except ValueError:
+        return []
+    except FileNotFoundError:
+        return []
+cats_info = get_cats_info("path.txt")
+print(cats_info)
