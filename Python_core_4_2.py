d581281189a5558299a19bbc8d3c74be74165fa1
Mari86957 <133572710+Mari86957@users.noreply.github.com>
2024-08-04 23:35:50 +0300

    Add files via upload

--- Python_core_4_2.py
+++ Python_core_4_2.py
@@ -1,20 +1,24 @@
 from pathlib import Path
 
-def get_cats_info(path):
+def get_cats_info(cats):
     cats_info = []
     try:
-        with open("path.txt", "r", encoding='utf-8') as fh:
+        with open(cats, "r", encoding='utf-8') as fh:
             for line in fh:
                 data = line.strip().split(',')
-                my_dict = {
-                    "id": data[0], 
-                    "name": data[1], 
-                    "age": data[2]}
-                cats_info.append(my_dict)
+                if len(data) == 3:
+                    my_dict = {
+                        "id": data[0], 
+                        "name": data[1], 
+                        "age": data[2]
+                        }
+                    cats_info.append(my_dict)
+                else:
+                    print("buu")
         return cats_info
     except ValueError:
         return []
     except FileNotFoundError:
         return []
-cats_info = get_cats_info("path.txt")
+cats_info = get_cats_info("./Module_4/cats.txt")
 print(cats_info)
